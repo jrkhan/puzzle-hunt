01afe05fa61004a3226c68fa9b68e0a55a834fc9
Jamil Khan <jamilrkhan@gmail.com>
2022-07-04 16:50:32 -0400

    Built in scanner

--- src/Scanner.js
+++ src/Scanner.js
@@ -1,56 +1,27 @@
 import {Html5Qrcode} from "html5-qrcode"
-import { useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 
 export default function Scanner() {
-    let [cameraId, setCameraId] = useState()
     let nav = useNavigate()
-    Html5Qrcode.getCameras().then(devices => {
-        /**
-         * devices would be an array of objects of type:
-         * { id: "id", label: "label" }
-         */
-        if (devices && devices.length) {
-            devices.forEach(({id, label}) => {
-                if (label === "environment") {
-                    setCameraId(id)
-                    return
-                }
-            })
-            setCameraId(devices[0].id)
-          // .. use this to start scanning.
-        }
-      }).catch(err => {
-        // handle err
-      });
-
+    let cb = useCallback(function navToDest(dest) {
+        nav(dest, {replace: true})
+    }, [nav])
+    
+    
     useEffect(() => {
-        if (!cameraId) {
-            return
-        }
-        
-        const html5QrCode = new Html5Qrcode("reader");
-        html5QrCode.start(
-        cameraId, 
-        {
-            fps: 10,    // Optional, frame per seconds for qr code scanning
-            //qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
-        },
-        (decodedText, decodedResult) => {
+
+        let qrCodeSuccessCallback = (decodedText, decodedResult) => {
             if (decodedText.startsWith("https://puzzlealley.com")) {
                 let path = decodedText.replace("https://puzzlealley.com", "")
-                nav(path, {replace: true})
+                cb(path)
             }
-        },
-        (errorMessage) => {
-            // parse error, ignore it.
-        })
-        .catch((err) => {
-        // Start failed, handle it.
-        });
-    }, [cameraId, nav])
+        };
+        const html5QrCode = new Html5Qrcode("reader");
+        html5QrCode.start({ facingMode: "environment" }, {fps: 10}, qrCodeSuccessCallback);
+    }, [cb])
    
 
-    return <div id="reader" width="600px"></div>
+    return <div id="reader" width="100%"></div>
 }
